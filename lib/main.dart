9cdf47de3e338dff8e09a35873aad7622f1f63ca
AKSHAYK0UL <akshaykoul999@gmail.com>
2024-12-02 02:26:16 +0530

    minor bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,29 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
+import 'package:nex_music/bloc/artist_bloc/bloc/artist_bloc.dart';
 import 'package:nex_music/bloc/auth_bloc/bloc/auth_bloc.dart';
+import 'package:nex_music/bloc/full_artist_songs_bloc/bloc/full_artist_bloc.dart';
+import 'package:nex_music/bloc/homesection_bloc/homesection_bloc.dart';
+import 'package:nex_music/bloc/playlist_bloc/playlist_bloc.dart';
+import 'package:nex_music/bloc/recent_played_bloc/bloc/recentplayed_bloc.dart';
+import 'package:nex_music/bloc/search_bloc/bloc/search_bloc.dart';
+import 'package:nex_music/bloc/searchedplaylist_bloc/bloc/searchedplaylist_bloc.dart';
+import 'package:nex_music/bloc/song_bloc/bloc/song_bloc.dart';
+import 'package:nex_music/bloc/song_dialog_bloc/bloc/song_dialog_bloc.dart';
+import 'package:nex_music/bloc/songstream_bloc/bloc/songstream_bloc.dart';
 import 'package:nex_music/bloc/user_logged_bloc/bloc/user_logged_bloc.dart';
+import 'package:nex_music/bloc/video_bloc/bloc/video_bloc.dart';
 import 'package:nex_music/core/bloc_provider/repository_provider/repository_provider.dart';
 import 'package:nex_music/core/route/route.dart';
 import 'package:nex_music/core/theme/theme.dart';
 import 'package:nex_music/presentation/auth/screens/auth_screen.dart';
 import 'package:nex_music/presentation/home/navbar/navbar.dart';
 import 'package:nex_music/repository/auth_repository/auth_repository.dart';
+import 'package:nex_music/repository/db_repository/db_repository.dart';
+import 'package:nex_music/repository/home_repo/repository.dart';
 
 import 'package:nex_music/secrets/firebase_options.dart';
 
@@ -29,14 +43,58 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   final RepositoryProviderClass repositoryProviderClassInstance;
-  MyApp(this.repositoryProviderClassInstance, {super.key});
+  const MyApp(this.repositoryProviderClassInstance, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
-      providers: [repositoryProviderClassInstance.authRepositoryProvider],
+      providers: [
+        repositoryProviderClassInstance.authRepositoryProvider,
+        repositoryProviderClassInstance.repositoryProvider,
+        repositoryProviderClassInstance.dbRepositoryProvider,
+      ],
       child: MultiBlocProvider(
         providers: [
+          BlocProvider(
+            create: (contexts) => HomesectionBloc(contexts.read<Repository>()),
+          ),
+          BlocProvider(
+            create: (contexts) => PlaylistBloc(contexts.read<Repository>()),
+          ),
+          BlocProvider(
+            create: (contexts) => SongstreamBloc(
+              contexts.read<Repository>(),
+              AudioPlayer(),
+              contexts.read<DbRepository>(),
+            ),
+          ),
+          BlocProvider(
+            create: (contexts) => SearchBloc(contexts.read<Repository>()),
+          ),
+          BlocProvider(
+            create: (contexts) => SongBloc(contexts.read<Repository>()),
+          ),
+          BlocProvider(
+            create: (contexts) => VideoBloc(contexts.read<Repository>()),
+          ),
+          BlocProvider(
+            create: (contexts) =>
+                SearchedplaylistBloc(contexts.read<Repository>()),
+          ),
+          BlocProvider(
+            create: (contexts) => ArtistBloc(contexts.read<Repository>()),
+          ),
+          BlocProvider(
+            create: (contexts) =>
+                RecentplayedBloc(contexts.read<DbRepository>()),
+          ),
+          BlocProvider(
+            create: (contexts) =>
+                FullArtistSongBloc(contexts.read<Repository>()),
+          ),
+          BlocProvider(
+            create: (contexts) => SongDialogBloc(contexts.read<DbRepository>()),
+          ),
           BlocProvider(
             create: (context) => AuthBloc(context.read<AuthRepository>()),
           ),
@@ -58,8 +116,9 @@ class MyApp extends StatelessWidget {
               if (isloggedIn) {
                 // Display when the user is logged in
                 return NavBar(
-                    repositoryProviderClassInstance:
-                        repositoryProviderClassInstance);
+                  repositoryProviderClassInstance:
+                      repositoryProviderClassInstance,
+                );
               } else {
                 return const AuthScreen(); // Display when the user is logged out
               }
--- lib/presentation/home/navbar/navbar.dart
+++ lib/presentation/home/navbar/navbar.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:nex_music/bloc/artist_bloc/bloc/artist_bloc.dart';
-import 'package:nex_music/bloc/full_artist_songs_bloc/bloc/full_artist_bloc.dart';
-import 'package:nex_music/bloc/homesection_bloc/homesection_bloc.dart';
-import 'package:nex_music/bloc/playlist_bloc/playlist_bloc.dart';
-import 'package:nex_music/bloc/recent_played_bloc/bloc/recentplayed_bloc.dart';
-import 'package:nex_music/bloc/search_bloc/bloc/search_bloc.dart';
-import 'package:nex_music/bloc/searchedplaylist_bloc/bloc/searchedplaylist_bloc.dart';
-import 'package:nex_music/bloc/song_bloc/bloc/song_bloc.dart';
-import 'package:nex_music/bloc/song_dialog_bloc/bloc/song_dialog_bloc.dart';
-
 import 'package:nex_music/bloc/songstream_bloc/bloc/songstream_bloc.dart';
-import 'package:nex_music/bloc/video_bloc/bloc/video_bloc.dart';
 import 'package:nex_music/core/bloc_provider/repository_provider/repository_provider.dart';
 import 'package:nex_music/presentation/audio_player/widget/miniplayer.dart';
 import 'package:nex_music/presentation/home/screen/home_screen.dart';
 import 'package:nex_music/presentation/recent/screens/recentscreen.dart';
-import 'package:nex_music/repository/db_repository/db_repository.dart';
-import 'package:nex_music/repository/home_repo/repository.dart';
 
 class NavBar extends StatefulWidget {
   final RepositoryProviderClass repositoryProviderClassInstance;
@@ -60,127 +46,78 @@ class _NavBarState extends State<NavBar> with WidgetsBindingObserver {
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.resumed) {
       context.read<SongstreamBloc>().add(UpdataUIEvent());
+      print("**********************Foreground********************");
     }
   }
 
   @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.sizeOf(context).height;
-    return MultiRepositoryProvider(
-      providers: [
-        widget.repositoryProviderClassInstance.repositoryProvider,
-        widget.repositoryProviderClassInstance.dbRepositoryProvider,
-      ],
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => HomesectionBloc(context.read<Repository>()),
-          ),
-          BlocProvider(
-            create: (context) => PlaylistBloc(context.read<Repository>()),
-          ),
-          BlocProvider(
-            create: (context) => SongstreamBloc(
-              context.read<Repository>(),
-              AudioPlayer(),
-              context.read<DbRepository>(),
-            ),
-          ),
-          BlocProvider(
-            create: (context) => SearchBloc(context.read<Repository>()),
-          ),
-          BlocProvider(
-            create: (context) => SongBloc(context.read<Repository>()),
-          ),
-          BlocProvider(
-            create: (context) => VideoBloc(context.read<Repository>()),
-          ),
-          BlocProvider(
-            create: (context) =>
-                SearchedplaylistBloc(context.read<Repository>()),
-          ),
-          BlocProvider(
-            create: (context) => ArtistBloc(context.read<Repository>()),
-          ),
-          BlocProvider(
-            create: (context) => RecentplayedBloc(context.read<DbRepository>()),
-          ),
-          BlocProvider(
-            create: (context) => FullArtistSongBloc(context.read<Repository>()),
-          ),
-          BlocProvider(
-            create: (context) => SongDialogBloc(context.read<DbRepository>()),
-          ),
-        ],
-        child: PopScope(
-          canPop: _selectedIndex == 0 ? true : false,
-          onPopInvokedWithResult: (_, __) {
-            if (_selectedIndex != 0) {
-              setState(
-                () {
-                  _selectedIndex = 0;
-                },
-              );
-            }
-          },
-          child: Scaffold(
-            body: IndexedStack(
-              index: _selectedIndex,
-              children: screens,
-            ),
-            bottomNavigationBar: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                MiniPlayer(screenSize: screenSize),
-                Theme(
-                  data: ThemeData(
-                    bottomNavigationBarTheme:
-                        Theme.of(context).bottomNavigationBarTheme,
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(top: screenSize * 0.0028),
-                    child: BottomNavigationBar(
-                      items: [
-                        BottomNavigationBarItem(
-                          icon: Icon(_selectedIndex == 0
-                              ? Icons.home
-                              : Icons.home_outlined),
-                          label: 'Home',
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Icon(_selectedIndex == 1
-                              ? Icons.music_note
-                              : Icons.music_note_outlined),
-                          label: 'Recent',
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Icon(_selectedIndex == 2
-                              ? Icons.playlist_play_rounded
-                              : Icons.playlist_play),
-                          label: 'Playlist',
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Icon(_selectedIndex == 3
-                              ? CupertinoIcons.heart_fill
-                              : CupertinoIcons.heart),
-                          label: 'Favorites',
-                        ),
-                      ],
-                      currentIndex: _selectedIndex,
-                      onTap: (index) {
-                        setState(() {
-                          _selectedIndex = index;
-                        });
-                      },
+  Widget build(BuildContext contexts) {
+    final screenSize = MediaQuery.sizeOf(contexts).height;
+    return PopScope(
+      canPop: _selectedIndex == 0 ? true : false,
+      onPopInvokedWithResult: (_, __) {
+        if (_selectedIndex != 0) {
+          setState(() {
+            _selectedIndex = 0;
+          });
+        }
+      },
+      child: Scaffold(
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: screens,
+        ),
+        bottomNavigationBar: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            MiniPlayer(screenSize: screenSize),
+            Theme(
+              data: ThemeData(
+                bottomNavigationBarTheme:
+                    Theme.of(context).bottomNavigationBarTheme,
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(top: screenSize * 0.0028),
+                child: BottomNavigationBar(
+                  items: [
+                    BottomNavigationBarItem(
+                      icon: Icon(_selectedIndex == 0
+                          ? Icons.home
+                          : Icons.home_outlined),
+                      label: 'Home',
                     ),
-                  ),
+                    BottomNavigationBarItem(
+                      icon: Icon(_selectedIndex == 1
+                          ? Icons.music_note
+                          : Icons.music_note_outlined),
+                      label: 'Recent',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(_selectedIndex == 2
+                          ? Icons.playlist_play_rounded
+                          : Icons.playlist_play),
+                      label: 'Playlist',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(_selectedIndex == 3
+                          ? CupertinoIcons.heart_fill
+                          : CupertinoIcons.heart),
+                      label: 'Favorites',
+                    ),
+                  ],
+                  currentIndex: _selectedIndex,
+                  onTap: (index) {
+                    setState(() {
+                      _selectedIndex = index;
+                    });
+                  },
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
